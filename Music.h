d710f69eb752735b2fbfa77044426c3ca2469e62
Ninja-Turtle-850 <144018086+Ninja-turtel-850@users.noreply.github.com>
2024-12-12 10:29:58 +0100

    MUSIC
    SOUND

--- Music.h
+++ Music.h
@@ -8,11 +8,13 @@ public:
 
 	void PlayMusic(std::string path);
 	void StopMusic();
+	void PlaySound(std::string path);
 private:
 	Music();
 	~Music() {};
 	static Music* instance;
 	sf::Music* music;
 	sf::Sound* sound;
+	sf::SoundBuffer* buffer;
 
 };
